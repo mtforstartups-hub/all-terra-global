2347c3893a7b80bca2fe5b522387e3d8df62e8b8
Pritam Dhara <mtforstartups@gmail.com>
2026-03-11 17:15:54 +0530

    updated to allow cors

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,31 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  // Temporary. Remove later.
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://all-terra-global.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
